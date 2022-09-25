a1fdf0f2e8395e20dcf6c1aea0b9c1cd1c6f9413
erisha <forever.erisha@gmail.com>
2022-09-25 02:54:27 -0400

    added category component. app is looking good.

--- ecommerce-store1/src/data.js
+++ ecommerce-store1/src/data.js
@@ -9,13 +9,13 @@ export const sliderItems = [
   {
     id: 2,
     img: 'https://i.imgur.com/Y8Mo1gA.jpg',
-    title: 'ZODIAC TEA CUPS',
+    title: 'ZODIAC TEACUPS',
     desc: "DON'T MISS OUT ON OUR GRAND OPENING SALE! 15% OFF YOUR 1ST ORDER!",
     bg: 'fcf1ed',
   },
   {
     id: 3,
-    img: 'https://i.imgur.com/1tbthF5.jpg',
+    img: 'https://i.imgur.com/ehQLHAS.jpg',
     title: 'ZODIAC MANIFESTING CANDLES',
     desc: "DON'T MISS OUT ON OUR GRAND OPENING SALE! 15% OFF YOUR 1ST ORDER!",
     bg: 'fbf0f4',
@@ -23,19 +23,19 @@ export const sliderItems = [
 ];
 
 export const categories = [
-    {
-        id: 1,
-        img: 'https://i.imgur.com/FfPMa0o.jpg',
-        title: 'ZODIAC TEAS' ,
-    },
-    {
-        id: 2,
-        img: 'https://i.imgur.com/Y8Mo1gA.jpg',
-        title: 'ZODIAC TEA CUPS',
-    },
-    {
-        id: 3,
-        img: 'https://i.imgur.com/1tbthF5.jpg',
-        title: 'ZODIAC CANDLES',
-    },
-]
+  {
+    id: 1,
+    img: 'https://i.imgur.com/VxIrvWS.jpg',
+    title: 'TEAS',
+  },
+  {
+    id: 2,
+    img: 'https://i.imgur.com/2vIpFbX.jpg',
+    title: 'Accessories',
+  },
+  {
+    id: 3,
+    img: 'https://i.imgur.com/58j2Mff.jpg',
+    title: 'CANDLES',
+  },
+];
--- ecommerce-store1/src/pages/Home.jsx
+++ ecommerce-store1/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import Announcement from '../components/Announcement'
+import Categories from '../components/Categories'
 import Navbar from '../components/Navbar'
 import Slider from '../components/Slider'
 
@@ -9,7 +10,8 @@ function Home() {
     <div>
         <Announcement />
         <Navbar />
-        <Slider/>
+        <Slider />
+        <Categories />
     </div>
   )
 }
