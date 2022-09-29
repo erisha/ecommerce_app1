bc289a85eebf587d16acc42ebef3aa09888f5d96
erisha <forever.erisha@gmail.com>
2022-09-29 09:47:47 -0400

    added products to home page & styled.

--- ecommerce-store1/src/data.js
+++ ecommerce-store1/src/data.js
@@ -16,7 +16,7 @@ export const sliderItems = [
   {
     id: 3,
     img: 'https://i.imgur.com/ehQLHAS.jpg',
-    title: 'ZODIAC MANIFESTING CANDLES',
+    title: 'ZODIAC CANDLES',
     desc: "DON'T MISS OUT ON OUR GRAND OPENING SALE! 15% OFF YOUR 1ST ORDER!",
     bg: 'fbf0f4',
   },
@@ -31,7 +31,7 @@ export const categories = [
   {
     id: 2,
     img: 'https://i.imgur.com/2vIpFbX.jpg',
-    title: 'Accessories',
+    title: 'CUPS & Thangs',
   },
   {
     id: 3,
@@ -39,3 +39,46 @@ export const categories = [
     title: 'CANDLES',
   },
 ];
+
+export const popularProducts = [
+    {
+        id: 1 ,
+        img: 'https://i.imgur.com/7Ng5PY7.jpg',
+
+    },
+    {
+        id: 2,
+        img: 'https://i.imgur.com/zaMX4c9.jpg',
+
+    },
+    {
+        id: 3,
+        img: 'https://i.imgur.com/r5LN9kD.jpg',
+
+    },
+    {
+        id: 4,
+        img: 'https://i.imgur.com/xiRl1nA.jpg',
+
+    },
+    {
+        id: 5,
+        img: 'https://i.imgur.com/pCuvBWu.jpg',
+
+    },
+    {
+        id: 6,
+        img: 'https://i.imgur.com/vh5JN9n.jpg',
+
+    },
+    {
+        id: 7,
+        img: 'https://i.imgur.com/hCjSZ8A.jpg',
+
+    },
+    {
+        id: 8,
+        img: 'https://i.imgur.com/6tXKlQb.jpg',
+
+    }
+]
