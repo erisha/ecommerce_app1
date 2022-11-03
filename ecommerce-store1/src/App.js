bcf9f7526db8fea6f4c24b78b4a20d2b35e9e9df
erisha <forever.erisha@gmail.com>
2022-11-02 22:08:37 -0400

    added Cart page

--- ecommerce-store1/src/App.js
+++ ecommerce-store1/src/App.js
@@ -4,10 +4,11 @@ import Home from './pages/Home';
 import ProductList from './pages/ProductList';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Cart from './pages/Cart';
 
 
 const App = () => {
-  return <Login />
+  return <Cart />
 }
 
 export default App;
