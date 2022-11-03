5abf82b2ef30f73e18f1985ad675493a4164a98b
erisha <forever.erisha@gmail.com>
2022-11-02 20:48:11 -0400

    added background to register page

--- ecommerce-store1/src/pages/Register.jsx
+++ ecommerce-store1/src/pages/Register.jsx
@@ -1,6 +1,29 @@
 import styled from "styled-components"
 
-const Container = styled.div``;
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+        rgba(255, 255, 255, 0.5),
+        rgba(255, 255, 255, 0.5)
+    ),
+    url("https://i.imgur.com/FfPMa0o.jpg")
+        center;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    `;
+
+const Wrapper = styled.div``;
+
+const Title = styled.h1``;
+
+const Form = styled.form``;
+
+const Input = styled.input``;
+
+const Button = styled.button``;
 
 const Register = () => {
   return (
